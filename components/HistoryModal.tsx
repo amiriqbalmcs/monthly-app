fd54ccb39a1a79e044591b4fb0fc3a317497f5b9
Amir Iqbal <48883764+amiriqbalmcs@users.noreply.github.com>
2025-08-23 19:34:50 +0500

    Updated HistoryModal.tsx

--- components/HistoryModal.tsx
+++ components/HistoryModal.tsx
@@ -381,7 +381,8 @@ const styles = StyleSheet.create({
     marginBottom: 16,
   },
   chart: {
-    marginVertical: 18,
+    marginVertical: 8,
+    marginHorizontal: 18,
     borderRadius: 16,
     alignSelf: 'center',
   },
